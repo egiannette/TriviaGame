e0658a0bde36a49fb502c71e6a32e52d37652a1a
Erin Giannette <emg4784@yahoo.com>
2016-08-31 20:35:24 -0500

    adding the working counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,17 @@ $("#container").hide();
 $("#startbtn").on("click", function(){
 	$("#startbtn").hide();
 	$("#container").show();
+	countDown(120, "status");
 })
+
+function countDown(secs, elem){
+	var element = document.getElementById(elem);
+	element.innerHTML = "Time left " +secs+ " seconds";
+	if (secs < 1) {
+		clearTimeout(timer);
+		// then I will hide the questions and show the results since the time is up
+
+	}
+	secs--;
+	var timer= setTimeout('countDown('+secs+',"'+elem+'")',1200);
+}
